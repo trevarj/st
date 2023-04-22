71b9d0d3c284e945c4622c4f6053fa84a6ee9c50
Trevor Arjeski <tmarjeski@gmail.com>
2023-04-22 15:04:32 +0300

    set font and borderpx

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetbrainsMono Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 16;
 
 /*
  * What program is execed by st depends of these precedence rules:
