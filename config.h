dec1a4995045826d1cca7bb89bd3bec7bd78f9a3
Trevor Arjeski <tmarjeski@gmail.com>
2023-05-03 21:34:11 +0300

    increase emoji font size

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetbrainsMono Nerd Font Mono:pixelsize=22:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = { "NotoColorEmoji:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 16;
 
 /*
